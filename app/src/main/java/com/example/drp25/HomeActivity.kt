ea79ccbcb65f2195c2362914702c7edc00f2673d
hettysymes <hettysymes@users.noreply.github.com>
2023-06-01 15:41:42 +0100

    Clicking mail button adds match entry to realtime database

--- app/src/main/java/com/example/drp25/HomeActivity.kt
+++ app/src/main/java/com/example/drp25/HomeActivity.kt
@@ -1,12 +1,10 @@
 package com.example.drp25
 
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import androidx.navigation.ui.AppBarConfiguration
 import com.example.drp25.databinding.ActivityHomeBinding
-import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.database.FirebaseDatabase
 
 class HomeActivity : AppCompatActivity() {
@@ -24,10 +22,8 @@ class HomeActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAnchorView(R.id.fab)
-                .setAction("Action") { matchesRef.setValue("Kevin") }.show()
+        binding.fab.setOnClickListener {
+            matchesRef.child("match2").setValue("Pierre")
         }
 
     }
