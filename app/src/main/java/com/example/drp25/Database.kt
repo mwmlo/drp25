bbd93090928f61abf5be3da0f6453e51acce029b
hettysymes <hettysymes@users.noreply.github.com>
2023-06-05 14:51:43 +0100

    added interest-related functions

--- app/src/main/java/com/example/drp25/Database.kt
+++ app/src/main/java/com/example/drp25/Database.kt
@@ -7,6 +7,7 @@ import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 
 private val unisRef = FirebaseDatabase.getInstance().getReference().child("universities")
+
 fun getData(ref: DatabaseReference, callback: (List<String>) -> Unit) {
     ref.addListenerForSingleValueEvent(object : ValueEventListener {
         override fun onDataChange(snapshot: DataSnapshot) {
@@ -31,4 +32,38 @@ fun getMatches(uniId: String, userId: String): List<String> {
     var retList: List<String> = listOf()
     getData(matchesRef) { dataList -> retList = dataList }
     return retList
+}
+// returns generated userId (or null if unsuccessful)
+fun addUser(uniId: String, name: String, nationality: String): String? {
+    val usersRef = unisRef.child(uniId).child("users")
+    val userId = usersRef.push().key
+    if (userId != null) {
+        val userRef = usersRef.child(userId)
+        userRef.child("name").setValue(name)
+        userRef.child("nationality").setValue(nationality)
+        userRef.child("interests").addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                TODO("Change matches lists if interests change")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+    }
+    return userId
+}
+
+fun addInterest(uniId: String, userId: String, interest: String) {
+    modifyInterest(uniId, userId, interest, true)
+}
+
+fun removeInterest(uniId: String, userId: String, interest: String) {
+    modifyInterest(uniId, userId, interest, false)
+}
+
+private fun modifyInterest(uniId: String, userId: String, interest: String, boolean: Boolean) {
+    val usersRef = unisRef.child(uniId).child("users").child(userId).child("interests")
+        .child(interest).setValue(boolean)
 }
