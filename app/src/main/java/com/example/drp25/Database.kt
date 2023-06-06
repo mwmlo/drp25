2441aac1e81d730893217d05a60ffa35bdcd062f
hettysymes <hettysymes@users.noreply.github.com>
2023-06-06 15:49:55 +0100

    add matching functions

--- app/src/main/java/com/example/drp25/Database.kt
+++ app/src/main/java/com/example/drp25/Database.kt
@@ -1,54 +1,109 @@
 package com.example.drp25
 
-import android.util.Log
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.GenericTypeIndicator
 import com.google.firebase.database.ValueEventListener
 import com.google.gson.Gson
-import org.json.JSONArray
 
 private val unisRef = FirebaseDatabase.getInstance().reference.child("universities")
 private val matcher: Matcher = BasicMatcher()
 private val gson = Gson()
 
-//private fun addMatch(uniId: String, user1Id: String, user2Id: String) {
-//    val usersRef = unisRef.child(uniId).child("users")
-//    val matches1Ref = usersRef.child(user1Id).child("matches")
-//    val matches2Ref = usersRef.child(user2Id).child("matches")
-//
-//    val matches1 = getList(matches1Ref)?.toMutableList()
-//    val matches2 = getList(matches2Ref)?.toMutableList()
-//    matches1?.add(user2Id)
-//    matches2?.add(user1Id)
-//
-//    matches1Ref.setValue(matches1)
-//    matches2Ref.setValue(matches2)
-//}
+private fun addMatch(uniId: String, user1Id: String, user2Id: String) {
+    val usersRef = unisRef.child(uniId).child("users")
+    val matches1Ref = usersRef.child(user1Id).child("matches")
+    val matches2Ref = usersRef.child(user2Id).child("matches")
 
-//private fun removeMatch(uniId: String, user1Id: String, user2Id: String) {
-//    val usersRef = unisRef.child(uniId).child("users")
-//    val matches1Ref = usersRef.child(user1Id).child("matches")
-//    val matches2Ref = usersRef.child(user2Id).child("matches")
-//
-//    val matches1 = getList(matches1Ref)?.toMutableList()
-//    val matches2 = getList(matches2Ref)?.toMutableList()
-//    matches1?.remove(user2Id)
-//    matches2?.remove(user1Id)
-//
-//    matches1Ref.setValue(matches1)
-//    matches2Ref.setValue(matches2)
-//}
+    matches1Ref.addListenerForSingleValueEvent(object : ValueEventListener {
+        override fun onDataChange(snapshot: DataSnapshot) {
+            val value = snapshot.getValue(String::class.java)
+            val matches = gson.fromJson(value, Array<String>::class.java).toMutableList()
+            if (!matches.contains(user2Id)) {
+                matches.add(user2Id)
+                matches1Ref.setValue(gson.toJson(matches))
+            }
+        }
+
+        override fun onCancelled(error: DatabaseError) {
+            TODO("Not yet implemented")
+        }
+    })
+
+    matches2Ref.addListenerForSingleValueEvent(object : ValueEventListener {
+        override fun onDataChange(snapshot: DataSnapshot) {
+            val value = snapshot.getValue(String::class.java)
+            val matches = gson.fromJson(value, Array<String>::class.java).toMutableList()
+            if (!matches.contains(user1Id)) {
+                matches.add(user1Id)
+                matches2Ref.setValue(gson.toJson(matches))
+            }
+        }
+
+        override fun onCancelled(error: DatabaseError) {
+            TODO("Not yet implemented")
+        }
+
+    })
+}
+
+private fun removeMatch(uniId: String, user1Id: String, user2Id: String) {
+    val usersRef = unisRef.child(uniId).child("users")
+    val matches1Ref = usersRef.child(user1Id).child("matches")
+    val matches2Ref = usersRef.child(user2Id).child("matches")
+
+    matches1Ref.addListenerForSingleValueEvent(object : ValueEventListener {
+        override fun onDataChange(snapshot: DataSnapshot) {
+            val value = snapshot.getValue(String::class.java)
+            val matches = gson.fromJson(value, Array<String>::class.java).toMutableList()
+            if (matches.remove(user2Id)) {
+                matches1Ref.setValue(gson.toJson(matches))
+            }
+        }
+
+        override fun onCancelled(error: DatabaseError) {
+            TODO("Not yet implemented")
+        }
+    })
+
+    matches2Ref.addListenerForSingleValueEvent(object : ValueEventListener {
+        override fun onDataChange(snapshot: DataSnapshot) {
+            val value = snapshot.getValue(String::class.java)
+            val matches = gson.fromJson(value, Array<String>::class.java).toMutableList()
+            if (matches.remove(user1Id)) {
+                matches2Ref.setValue(gson.toJson(matches))
+            }
+        }
+
+        override fun onCancelled(error: DatabaseError) {
+            TODO("Not yet implemented")
+        }
+
+    })
+}
 
 fun updateMatches(uniId: String, userId: String, snapshot: DataSnapshot) {
     val usersRef = unisRef.child(uniId).child("users")
     usersRef.addListenerForSingleValueEvent(object : ValueEventListener {
         override fun onDataChange(dataSnapshot: DataSnapshot) {
-            for (child : dataSnapshot.children) {
-                val otherKey = child.key
-                //
+            for (child in dataSnapshot.children) {
+                val otherId = child.key
+                if (otherId != userId) {
+                    otherId?.let { usersRef.child(it) }
+                        ?.addListenerForSingleValueEvent(object : ValueEventListener {
+                            override fun onDataChange(otherSnapshot: DataSnapshot) {
+                                if (matcher.isMatch(snapshot, otherSnapshot)) {
+                                    addMatch(uniId, userId, otherId)
+                                } else {
+                                    removeMatch(uniId, userId, otherId)
+                                }
+                            }
+
+                            override fun onCancelled(error: DatabaseError) {
+                                TODO("Not yet implemented")
+                            }
+                        })
+                }
             }
         }
 
@@ -56,24 +111,7 @@ fun updateMatches(uniId: String, userId: String, snapshot: DataSnapshot) {
             //TODO
         }
     })
-
-//    var userKeys = getKeyData(usersRef)
-//
-//    for (otherUserId in userKeys) {
-//        if (otherUserId == userId) continue
-//        val otherUserRef = usersRef.child(otherUserId)
-//        if (getSnapshot(otherUserRef)?.let { matcher.isMatch(snapshot, it) } == true) {
-//            addMatch(uniId, userId, otherUserId)
-//        } else {
-//            removeMatch(uniId, userId, otherUserId)
-//        }
-//    }
 }
-
-//fun getMatches(uniId: String, userId: String): List<String> {
-//    val matchesRef = unisRef.child(uniId).child("users").child(userId).child("matches")
-//    return getValueData(matchesRef)
-//}
 
 // returns generated userId (or null if unsuccessful)
 fun addUser(uniId: String, name: String, nationality: String): String? {
@@ -129,8 +167,3 @@ fun removeInterest(uniId: String, userId: String, oldInterest: String) {
         }
     })
 }
-
-//private fun modifyInterest(uniId: String, userId: String, interest: String, boolean: Boolean) {
-//    val usersRef = unisRef.child(uniId).child("users").child(userId).child("interests")
-//        .child(interest).setValue(boolean)
-//}
