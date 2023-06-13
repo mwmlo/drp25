31f4534ad636428b206842a29fdb7e1eee89b7bb
Michelle Lo <lomichelle42@gmail.com>
2023-06-13 11:56:49 +0100

    Allow different hardcoded users to log in

---
+++ app/src/main/java/com/example/drp25/UserActivity.kt
@@ -0,0 +1,34 @@
+package com.example.drp25
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.example.drp25.ChatActivity.Companion.setCurrentUser
+
+const val UNI_ID = "imperialId"
+var USER_ID = ""
+
+class UserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user)
+
+        val maxButton = findViewById<Button>(R.id.enter_max_button)
+        val kevinButton = findViewById<Button>(R.id.enter_kevin_button)
+
+        maxButton.setOnClickListener {
+            USER_ID = "-NXPnWryIGR2S5aJmSGH"
+            setCurrentUser("max")
+            val intent = Intent(this, ChatActivity::class.java)
+            startActivity(intent)
+        }
+
+        kevinButton.setOnClickListener {
+            USER_ID = "-NXPnWs-phdiaSN_S87V"
+            setCurrentUser("kevin")
+            val intent = Intent(this, ChatActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
