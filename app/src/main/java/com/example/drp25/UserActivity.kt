00f7765f44d5860d132daa81f97c3869856185b4
Michelle Lo <lomichelle42@gmail.com>
2023-06-13 12:23:43 +0100

    Fix user ID for dummy users

--- app/src/main/java/com/example/drp25/UserActivity.kt
+++ app/src/main/java/com/example/drp25/UserActivity.kt
@@ -3,6 +3,7 @@ package com.example.drp25
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import com.example.drp25.ChatActivity.Companion.setCurrentUser
 
@@ -18,7 +19,7 @@ class UserActivity : AppCompatActivity() {
         val kevinButton = findViewById<Button>(R.id.enter_kevin_button)
 
         maxButton.setOnClickListener {
-            USER_ID = "-NXPnWryIGR2S5aJmSGH"
+            USER_ID = "-NXPnWs-phdiaSN_S87V"
             setCurrentUser("max")
             listenToUser(UNI_ID, USER_ID)
             val intent = Intent(this, ChatActivity::class.java)
@@ -26,7 +27,7 @@ class UserActivity : AppCompatActivity() {
         }
 
         kevinButton.setOnClickListener {
-            USER_ID = "-NXPnWs-phdiaSN_S87V"
+            USER_ID = "-NXPnWryIGR2S5aJmSGH"
             setCurrentUser("kevin")
             listenToUser(UNI_ID, USER_ID)
             val intent = Intent(this, ChatActivity::class.java)
