1d986b3f5842e4a4779967d21775d6a91f9735be
Hazel Wright <hgw21@ic.ac.uk>
2023-06-14 14:54:25 +0100

    Added new activity CommitteeEventsActivity.

---
+++ app/src/main/java/com/example/drp25/CommitteeEventsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.drp25
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CommitteeEventsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_committee_events)
+    }
+}
