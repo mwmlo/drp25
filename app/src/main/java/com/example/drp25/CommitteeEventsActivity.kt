c47ea188bbc21b92e140f73ee928419a250191ed
Hazel Wright <hgw21@ic.ac.uk>
2023-06-14 16:12:54 +0100

    Implemented functionality for buttons, so it's possible to navigate through committee activities.

--- app/src/main/java/com/example/drp25/CommitteeEventsActivity.kt
+++ app/src/main/java/com/example/drp25/CommitteeEventsActivity.kt
@@ -1,11 +1,24 @@
 package com.example.drp25
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class CommitteeEventsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_committee_events)
+
+        val toProfile = findViewById<Button>(R.id.to_profile_button)
+        toProfile.setOnClickListener {
+            val intent = Intent(this, CommitteeProfileActivity::class.java)
+            startActivity(intent)
+        }
+
+        val newEvent = findViewById<Button>(R.id.new_event_button)
+        newEvent.setOnClickListener {
+            // Create new card & populate
+        }
     }
 }
