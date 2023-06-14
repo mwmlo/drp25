668e73b30f9b67a6eb28a1953930e5c86285be2c
Hazel Wright <hgw21@ic.ac.uk>
2023-06-14 14:34:25 +0100

    Created new activity CommitteeProfileActivity.

---
+++ app/src/main/java/com/example/drp25/CommitteeProfileActivity.kt
@@ -0,0 +1,11 @@
+package com.example.drp25
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CommitteeProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_committee_profile)
+    }
+}
