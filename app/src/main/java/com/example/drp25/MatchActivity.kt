78c972aed47f77b8f25c46650de6c7e760af75fd
hettysymes <hettysymes@users.noreply.github.com>
2023-06-15 11:29:23 +0100

    changed colours so can work in dark mode, updated match view to be more similar to profile view

--- app/src/main/java/com/example/drp25/MatchActivity.kt
+++ app/src/main/java/com/example/drp25/MatchActivity.kt
@@ -2,13 +2,17 @@ package com.example.drp25
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.RatingBar
 import android.widget.ScrollView
+import android.widget.TableLayout
+import android.widget.TableRow
 import android.widget.TextView
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
@@ -25,13 +29,15 @@ class MatchActivity : AppCompatActivity() {
         override fun notify(matchIds: Set<String>) {
             parentLayout.removeAllViews()
             for (matchId in matchIds) {
-                val scrollView = ScrollView(context)
+                val inflater = LayoutInflater.from(this@MatchActivity)
+                val scrollView = inflater.inflate(R.layout.match_entry_view, parentLayout, false) as ScrollView
                 parentLayout.addView(scrollView)
-                val linearLayout = LinearLayout(context)
-                linearLayout.orientation = LinearLayout.VERTICAL
-                linearLayout.setPadding(30, 30, 30, 30)
-//                parentLayout.addView(linearLayout)
-                scrollView.addView(linearLayout)
+                val pfpImage = scrollView.findViewById<ImageView>(R.id.match_pfp_view)
+                val nameText = scrollView.findViewById<TextView>(R.id.name_view)
+                val infoText = scrollView.findViewById<TextView>(R.id.info_view)
+                val interestsTable = scrollView.findViewById<TableLayout>(R.id.interests_view)
+                val matchWithBtn = scrollView.findViewById<Button>(R.id.match_with_button)
+
                 val matchRef = FirebaseDatabase.getInstance().reference.child("universities")
                     .child(UNI_ID).child("users").child(matchId)
                 matchRef.addListenerForSingleValueEvent(object: ValueEventListener {
@@ -41,18 +47,24 @@ class MatchActivity : AppCompatActivity() {
                         val course = snapshot.child("course").value
                         val year = snapshot.child("year").value
 
-                        addText(linearLayout, "Name: $name")
-                        addText(linearLayout, "Nationality: $nationality")
-                        addText(linearLayout, "Course: $course")
-                        addText(linearLayout, "Year: $year")
-
-                        addText(linearLayout, "Interests:")
-                        for (interest in snapshot.child("interests").children) {
-                            addText(linearLayout, interest.key.toString())
+                        val pfpPath = snapshot.child("pfp").value
+                        if (pfpPath != null) {
+                            val imageBitmap = BitmapFactory.decodeFile(pfpPath as String)
+                            pfpImage.setImageBitmap(imageBitmap)
                         }
 
-                        val button = getButton(linearLayout, "Match with $name")
-                        button.setOnClickListener {
+                        nameText.text = name
+                        infoText.text = "Year $year | $course | $nationality"
+
+                        for (interest in snapshot.child("interests").children) {
+                            val rowView = inflater.inflate(R.layout.profile_interest, interestsTable, false) as TableRow
+                            val interestNameView = rowView.findViewById<TextView>(R.id.interest_name)
+                            interestNameView.text = interest.key
+                            interestsTable.addView(rowView)
+                        }
+
+                        matchWithBtn.text = "Match with $name"
+                        matchWithBtn.setOnClickListener {
                             selectedMatchId = matchId
                             selectedMatchName = name
                             val intent = Intent(this@MatchActivity, StampActivity::class.java)
@@ -60,31 +72,13 @@ class MatchActivity : AppCompatActivity() {
                             intent.putExtra("selectedMatchName", selectedMatchName)
                             startActivity(intent)
                         }
-                        linearLayout.addView(button)
                     }
 
                     override fun onCancelled(error: DatabaseError) {
                     }
-
                 })
             }
         }
-    }
-
-    private fun addText(linearLayout: LinearLayout, text: String) {
-        val entry = TextView(context)
-        entry.text = text
-        entry.setPadding(16, 16, 16, 16)
-        entry.textSize = 20f
-        linearLayout.addView(entry)
-    }
-
-    private fun getButton(linearLayout: LinearLayout, text: String): Button {
-        val btn = Button(context)
-        btn.text = text
-        btn.setPadding(16, 16, 16, 16)
-        btn.textSize = 20f
-        return btn
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
